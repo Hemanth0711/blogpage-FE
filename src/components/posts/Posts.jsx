c6cf9097888fbfea2cdfa96e8748439cf191d7f2
Hemanth0711 <hemanthsai0711@gmail.com>
2023-09-12 20:39:11 +0530

    changes

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -6,11 +6,11 @@ export default function Posts({posts}) {
   console.log(posts);
   return (
     <div className = "posts">
-      {
+      {/* {
         posts.map((post) => (
           <Post key = {post._id} post = {post} />
         ))
-      }
+      } */}
     </div>
   )
 }
