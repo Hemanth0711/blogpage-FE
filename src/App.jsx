2c813da610ac407771db24d32490aeee7941457d
Hemanth0711 <hemanthsai0711@gmail.com>
2023-09-13 22:29:31 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Register from './pages/register/Register';
 import { useContext } from 'react';
 import { Context } from './context/Context';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 
 function App() {
   const {user} = useContext(Context);
@@ -22,6 +22,7 @@ function App() {
       {/* <Write/> */}
       {/* <Settings /> */}
       {/* <Login /> */}
+      <HashRouter>
       <Router>
         <Topbar />
         <Routes>
@@ -38,6 +39,7 @@ function App() {
           <Route path ="/post/:postid" element={<Single />}/>
         </Routes>
       </Router>
+      </HashRouter>
     </div>
   )
 }
