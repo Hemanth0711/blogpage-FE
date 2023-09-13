038fa0625c1d95f5b6bbcd29bf0d8ba8e4297044
Hemanth0711 <hemanthsai0711@gmail.com>
2023-09-13 22:58:26 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Register from './pages/register/Register';
 import { useContext } from 'react';
 import { Context } from './context/Context';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const {user} = useContext(Context);
